6fbfa2789f5c62ca45dea880849fb4f93bb99d7d
Joaquin Beltran <j.beltranbaldy@gmail.com>
2022-05-25 21:24:31 +0200

    smol steps

--- bldrdsh/historical.py
+++ bldrdsh/historical.py
@@ -1,2 +1,31 @@
-def generate_historical():
+import os
+import pathlib
+import datetime
+
+def generate_historical(params: dict):
+
+    path = str(pathlib.Path().resolve())
+    current_time = datetime.datetime.now().strftime("%Y%M%d%H%M%S")
+    project_name = params.pop('project_name')
+    os.mkdir(path +'/'+ current_time + '_' + project_name )
+
+    metadata = generate_metadata_file(params)
+    
+    return f"The directory '{project_name}' has been created!"
+
+def generate_metadata_file(params):
+    """
+    Given initial params, call functions to generate
+    historical outputs.
+    Big JSON file.
+    """
+    quantity = params.pop('quantity')
+    to_json = "add date, time, title, quantity to json file"
+    return "return a summary of to_json but beautify it"
+
+def generate_table():
+    """
+    Base thing table type, all tables share same "schema".
+    Create CSV for now for all tables in sales
+    """
     pass
